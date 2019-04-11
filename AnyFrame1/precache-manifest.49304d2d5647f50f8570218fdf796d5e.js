a2b7c3991993958f5c6d625fff4f540379591854
chenga@wix.com <chenga@wix.com>
2019-04-12 01:00:41 +0300

    Updates

--- AnyFrame1/precache-manifest.49304d2d5647f50f8570218fdf796d5e.js
+++ AnyFrame1/precache-manifest.49304d2d5647f50f8570218fdf796d5e.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "https://chengawix.github.io/frame-god/AnyFrame1/static/js/runtime~main.08533b84.js"
   },
   {
-    "revision": "b8cff671d8a4736c588d",
-    "url": "https://chengawix.github.io/frame-god/AnyFrame1/static/js/main.1148a992.chunk.js"
+    "revision": "4ca31e0422491a5a96ea",
+    "url": "https://chengawix.github.io/frame-god/AnyFrame1/static/js/main.7404ed0c.chunk.js"
   },
   {
     "revision": "45124f853ba07aa965c0",
     "url": "https://chengawix.github.io/frame-god/AnyFrame1/static/js/2.6efc73d3.chunk.js"
   },
   {
-    "revision": "b8cff671d8a4736c588d",
+    "revision": "4ca31e0422491a5a96ea",
     "url": "https://chengawix.github.io/frame-god/AnyFrame1/static/css/main.1346d839.chunk.css"
   },
   {
-    "revision": "18d0d6f966825d70e05bd57d814432a9",
+    "revision": "e2b2bc5cf692d3a260d555adc21c1b01",
     "url": "https://chengawix.github.io/frame-god/AnyFrame1/index.html"
   }
 ];
--- AnyFrame1/service-worker.js
+++ AnyFrame1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://chengawix.github.io/frame-god/AnyFrame1/precache-manifest.46617f5417db5b1d6a7c05e6e41c6ff9.js"
+  "https://chengawix.github.io/frame-god/AnyFrame1/precache-manifest.49304d2d5647f50f8570218fdf796d5e.js"
 );
 
 workbox.clientsClaim();
