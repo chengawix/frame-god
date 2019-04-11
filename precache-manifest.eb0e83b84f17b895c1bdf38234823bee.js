bd4d741f16f4c2227a3025c9dd75812596306a46
chenga@wix.com <chenga@wix.com>
2019-04-11 22:26:59 +0300

    Updates

---
+++ precache-manifest.eb0e83b84f17b895c1bdf38234823bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "44dbd0ca1022d4be3e36",
+    "url": "/static/js/main.84c67fe6.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "44dbd0ca1022d4be3e36",
+    "url": "/static/css/main.1346d839.chunk.css"
+  },
+  {
+    "revision": "e0573f84d971bca429f7dc5f2172a4fe",
+    "url": "/index.html"
+  }
+];
