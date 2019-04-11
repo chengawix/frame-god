2abc334903a86d0c3f71303f0223864d27ce7d0b
chenga@wix.com <chenga@wix.com>
2019-04-11 22:51:23 +0300

    Updates

---
+++ precache-manifest.d6a9b93d1aa3f26a84f26c2ecd91b13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frame-god/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "82dbe12f542bdc5c44c8",
+    "url": "/frame-god/static/js/runtime~main.c5ad85e4.js"
+  },
+  {
+    "revision": "1b34477364d7b76f8566",
+    "url": "/frame-god/static/js/main.9a7f0942.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/frame-god/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1b34477364d7b76f8566",
+    "url": "/frame-god/static/css/main.1346d839.chunk.css"
+  },
+  {
+    "revision": "d7c212d86933894037819f675ccf356a",
+    "url": "/frame-god/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f21ef4468bed7d1baf4e1a3dcc920b32.js"
+  "/frame-god/precache-manifest.d6a9b93d1aa3f26a84f26c2ecd91b13a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/frame-god/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
