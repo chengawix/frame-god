68b9eeedf11f31739efef71d54ba0957754f694c
chenga@wix.com <chenga@wix.com>
2019-04-11 23:16:47 +0300

    Updates

---
+++ AnyFrame1/precache-manifest.8ccc24c7f93c11783631399e31316573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://chengawix.github.io/frame-god//AnyFrame1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "64b6b86c9183596b8f64",
+    "url": "https://chengawix.github.io/frame-god//AnyFrame1/static/js/runtime~main.04abc6d9.js"
+  },
+  {
+    "revision": "0a45bc0975fdcfd07880",
+    "url": "https://chengawix.github.io/frame-god//AnyFrame1/static/js/main.38835837.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "https://chengawix.github.io/frame-god//AnyFrame1/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0a45bc0975fdcfd07880",
+    "url": "https://chengawix.github.io/frame-god//AnyFrame1/static/css/main.1346d839.chunk.css"
+  },
+  {
+    "revision": "96bdba93d2a91d1a362d8ebcf8c0d800",
+    "url": "https://chengawix.github.io/frame-god//AnyFrame1/index.html"
+  }
+];
