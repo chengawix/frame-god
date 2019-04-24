762bcefcd1e31e29fe124905ab84572057de28ec
chenga@wix.com <chenga@wix.com>
2019-04-24 15:49:16 +0300

    Updates

---
+++ jira-md-editor/precache-manifest.8f541b20b96b3b8d64377ba0034eb6e7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a936ac9bec344b5aba64ce18a765e113",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/bold.a936ac9b.svg"
+  },
+  {
+    "revision": "2b93c35a2fea777ac6ba",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/css/main.7ab8ea3d.chunk.css"
+  },
+  {
+    "revision": "2520e420996d717120ad",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/js/runtime~main.658f13e1.js"
+  },
+  {
+    "revision": "2b475e3e72d24b82b5d33aa72fe4093e",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/list-ul.2b475e3e.svg"
+  },
+  {
+    "revision": "02b6e4b1936b55e32b81",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/js/2.65ad5159.chunk.js"
+  },
+  {
+    "revision": "636e15d7e854b6705c6b925af1288b30",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/italic.636e15d7.svg"
+  },
+  {
+    "revision": "7ce551e2919fdf54d71a3f10fa9aeee5",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/subscript.7ce551e2.svg"
+  },
+  {
+    "revision": "4c5704e87ba7e8050d392884fdecee6f",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/underline.4c5704e8.svg"
+  },
+  {
+    "revision": "2b93c35a2fea777ac6ba",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/js/main.955826bc.chunk.js"
+  },
+  {
+    "revision": "f8b9fa557d36ea370a6d443893e494a5",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/superscript.f8b9fa55.svg"
+  },
+  {
+    "revision": "51b4fac779638129f9fc5e4c6d932655",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/strikethrough.51b4fac7.svg"
+  },
+  {
+    "revision": "9d58f75b529db935797fae0c53c7737a",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/list-ol.9d58f75b.svg"
+  },
+  {
+    "revision": "cd867c47c359a6e733aecf256a95499e",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/text-color.cd867c47.svg"
+  },
+  {
+    "revision": "17ac1be249b6a4fc3afefcce6a2520e2",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/media/emoji.17ac1be2.svg"
+  },
+  {
+    "revision": "02b6e4b1936b55e32b81",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/static/css/2.4282dde1.chunk.css"
+  },
+  {
+    "revision": "d1bdadb5873637a526098b87f2805f99",
+    "url": "https://chengawix.github.io/frame-god/jira-md-editor/index.html"
+  }
+];
---
+++ jira-md-editor/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://chengawix.github.io/frame-god/jira-md-editor/precache-manifest.8f541b20b96b3b8d64377ba0034eb6e7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://chengawix.github.io/frame-god/jira-md-editor/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
